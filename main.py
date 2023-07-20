bbe3deaa6a20d797dd556eca2d998af6de744863
Dmitry Lifenko <lifenkodi@gmail.com>
2023-07-20 18:17:06 +0700

    Add docstring

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""Программа для выбора имени и класса персонажа.
+Проверяет навыки в тренировке.
+"""
 from random import randint
-from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Описывает наносимый персонажем урон при атаке.
+    Зависит от выбранного класса.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -16,6 +21,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Описывает защиту персонажа. Зависит от выбранного класса."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -26,6 +32,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Описывает специальное умение персонажа.
+    Зависит от выбранного класса.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -37,6 +46,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Проверка атаки, защиты и специального умения."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +71,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Позволяет выбрать класс персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,8 +93,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-if __name__ == '__main__':
-    run_screensaver()
+def main():
+    """Основная функция, запускающая оставльные."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
